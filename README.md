738e222db9480cae845bba9cb7ae6786442f60a3
szskr <seizosakurai@mac.com>
2019-08-31 16:12:01 +0900

    impro

--- README.md
+++ README.md
@@ -3,10 +3,15 @@
 Studing notes and codes for lisp beginners.
 
 References are:
-  (1) "Lisp For Beginners" by Ikuo Takeuchi, 
+-- (1) "Lisp For Beginners" by Ikuo Takeuchi, 
 
-src/ch13.lisp: Outline of eval function.
-  From (1), chapter 13.
+FILES:
+src/ch13.lisp:
+-- Outline of eval function.
+-- From (1), chapter 13.
+
+src/microlisp.lisp
+-- writing a small micro lisp in lisp
 
   
 
---
+++ src/microlisp.lisp
@@ -0,0 +1,21 @@
+;;;
+;;; Experimental Micro Lisp
+;;;
+
+;;
+;; micro-eval
+;;
+(defun micro-eval (s environment)
+  (cond ((atom s)
+	 (cond ((equal s t) t)
+	       ((equal s nil) nil)
+	       ((number s) s)
+	       (t (micro-valu s environment))))
+	((equal (car s) 'm-quote) (cadr s))
+	((equal (car s) 'm-cond)
+	 (micro-evalcond (cdr s) environment))
+	(t (micro-apply (car s)
+			(mapcar '(lambda (x)
+				   (micro-eval x environment))
+				(cdr s))
+			environment))))
