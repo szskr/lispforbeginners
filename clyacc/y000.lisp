f9c3188d8393698fd26b9dcd9d0069c303b0993d
szskr <seizosakurai@mac.com>
2019-09-18 21:17:22 +0900

    IMPRO

--- clyacc/y000.lisp
+++ clyacc/y000.lisp
@@ -2,11 +2,26 @@
 ;;; Prerequisites
 ;;;
 
+(defun lm () (load "./y000.lisp"))
+  
 (deftype index () '(unsigned-byte 14))
 (deftype signed-index () '(signed-byte 15))
 
-(defstruct person
-  name
-  age
-  hobbies)
+;;
+;; function arguments
+;;   &optional
+;;   &rest
+;;   &key
+;;
+(defun foo_opt (a b &optional (c 100) (d 200))
+  (format t "a = ~a, b = ~a, c = ~a, d = ~a" a b c d))
 
+(defun foo_rest (a &rest values)
+  (format t "a = ~a values = ~a" a values))
+
+(defun foo_key (&key a b c d)
+  (format t "a = ~a b= ~a  c = ~a d = ~a" a b c d))
+
+(defun foo_key_def (&key (a 10) b (c 100) d)
+    (format t "a = ~a b= ~a  c = ~a d = ~a" a b c d))
+ 
