67348056180a6ad863aa83a5777303aa1173c58e
szskr <seizosakurai@mac.com>
2019-09-19 10:38:52 +0900

    IMPRO

--- clyacc/y000.lisp
+++ clyacc/y000.lisp
@@ -23,5 +23,36 @@
   (format t "a = ~a b= ~a  c = ~a d = ~a" a b c d))
 
 (defun foo_key_def (&key (a 10) b (c 100) d)
-    (format t "a = ~a b= ~a  c = ~a d = ~a" a b c d))
- 
+  (format t "a = ~a b= ~a  c = ~a d = ~a" a b c d))
+
+;;
+;; On Structure
+;;
+
+(defstruct person00
+  name
+  age
+  hobbies)
+
+(defstruct (person01)
+  (name)
+  age
+  hobbies)
+
+(setq *KenT* (make-person00 :name "Ken Thompson" :age 67 :hobbies '(programming unix)))
+(setq *Dennis* (make-person01 :name "Dennis Richie" :age 100 :hobbies '(programming C)))
+
+;;;
+;;; Option :constructor
+;;;
+;;; This option takes one argument, a symbol, which specifies the name of the constructor
+;;; function. If the argument is not provided or if the option itself is not provided,
+;;; the name of the constructor is produced by concatenating the string "MAKE-" and the name
+;;; of the structure, putting the name in whatever package is current at the time the
+;;; defstruct form is processed (see *package*). If the argument is provided and is nil,
+;;; no constructor function is defined.
+;;;
+(defstruct (person10)
+  name
+  age
+  hobbies)
