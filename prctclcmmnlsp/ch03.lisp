166343881fdde7b7ed82cbdcaa5233768db031aa
szskr <seizosakurai@mac.com>
2019-11-18 22:03:16 +0900

    IMPRO

--- prctclcmmnlsp/ch03.lisp
+++ prctclcmmnlsp/ch03.lisp
@@ -1,3 +1,19 @@
 ;;
 ;; Practical Common Lisp
 ;;
+
+(defvar *db* nil)
+
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist rating rating :ripped ripped))
+
+(defun add-record(cd) (push cd *db*))
+
+(defun dump-db ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a:" prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
