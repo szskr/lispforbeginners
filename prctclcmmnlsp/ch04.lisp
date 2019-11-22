b0bc84cd398c21a03b4843cde3d0caadc35a5ce4
szskr <seizosakurai@mac.com>
2019-11-22 13:41:33 +0900

    IMPRO

--- prctclcmmnlsp/ch04.lisp
+++ prctclcmmnlsp/ch04.lisp
@@ -9,3 +9,20 @@
 ;;  &rest: rest parameters
 ;;  &key: keyword parameters
 ;;
+
+;;
+;; Functions As Data
+;;
+(defun foo4-1 ()
+  (format t "Function foo4-1"))
+
+(function foo4-1)
+(funcall #'foo4-1)   ;; indirect invocation
+(apply #'foo4-1 ())
+
+(defun plot (fn min max step)
+  (loop for i from min to max by step do
+	(loop repeat (funcall fn i) do (format t "*"))
+	(format t "~%")))
+
+(plot #'exp 0 4 1/2)
