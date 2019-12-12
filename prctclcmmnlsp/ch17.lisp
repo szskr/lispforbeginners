86865136c2f686471373aa16d42b658111387f33
szskr <seizosakurai@mac.com>
2019-12-12 13:50:44 +0900

    IMPRO

--- prctclcmmnlsp/ch17.lisp
+++ prctclcmmnlsp/ch17.lisp
@@ -16,9 +16,10 @@
 (defparameter *account-0* (make-instance 'bank-account-0))
 (setf (slot-value *account-0* 'customer-name) "John Doe")
 (setf (slot-value *account-0* 'balance) 1000)
-(format t "account = ~a~%" *account-0*)
+(format t "account-0 = ~a~%" *account-0*)
 (format t "customer-name = ~a~%" (slot-value *account-0* 'customer-name))
 (format t "balance       = ~a~%" (slot-value *account-0* 'balance))
+(format t "~%")
 
 ;;;
 ;;; Object Initialization
@@ -28,5 +29,25 @@
 ;;;    1.3) by definig a method on the generic function INITIALIZE-INSTANCE,
 ;;;                                                     which is called by make-instance
 ;;;
+(defclass bank-account-1 ()
+  ((customer-name
+    :initarg :customer-name)
+   (balance
+    :initarg :balance
+    :initform 10)))
 
+(defparameter *account-1*
+  (make-instance 'bank-account-1
+		 :customer-name "John Doe"
+		 :balance 1000))
+(format t "account-1 = ~a~%" *account-1*)
+(format t "customer-name = ~a~%" (slot-value *account-1* 'customer-name))
+(format t "balance       = ~a~%" (slot-value *account-1* 'balance))
+(format t "~%")
 
+(defparameter *account-11*
+  (make-instance 'bank-account-1
+		 :customer-name "John Doe"))
+(format t "account-11 = ~a~%" *account-11*)
+(format t "customer-name = ~a~%" (slot-value *account-11* 'customer-name))
+(format t "balance       = ~a~%" (slot-value *account-11* 'balance))
