be1b92efc9ebbe882f3c371ec816938942e8eba5
szskr <seizosakurai@mac.com>
2019-12-13 09:04:39 +0900

    IMPRO

--- prctclcmmnlsp/ch05.lisp
+++ prctclcmmnlsp/ch05.lisp
@@ -28,4 +28,18 @@
 (format t "(foo-3 :a 1 :b 2 :c 3) = ~a~%" (foo-3 :a 1 :b 2 :c 3))
 (format t "~%")
 
+(defun foo-4 (&key (a 0) (b 10) (c (+ a b)))
+  (list a b c))
+(format t "(foo-4 :a 1)             = ~a~%" (foo-4 :a 1))
+(format t "(foo-4 :b 2)             = ~a~%" (foo-4 :b 2))
+(format t "(foo-4 :a 1 :b 2 :c 100) = ~a~%" (foo-4 :a 1 :b 2 :c 100))
+(format t "~%")
+
+(defun foo-5 (a b &rest params)
+  (list a b params))
+(format t "(foo-5 1 2)         = ~a~%" (foo-5 1 2))
+(format t "(foo-5 1 2 3)       = ~a~%" (foo-5 1 2 3))
+(format t "(foo-5 1 2 3 4)     = ~a~%" (foo-5 1 2 3 4))
+(format t "~%")
+
 (defparameter *fn5-1* (let ((count 0)) #'(lambda () (setf count (+ 1 count))))) ;; Closure!
