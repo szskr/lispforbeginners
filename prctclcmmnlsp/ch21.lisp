b11d544b39ce2ffc8be3be4fc9b0b5f474a809d0
szskr <seizosakurai@mac.com>
2019-12-17 22:43:37 +0900

    IMPRO

--- prctclcmmnlsp/ch21.lisp
+++ prctclcmmnlsp/ch21.lisp
@@ -9,8 +9,32 @@
 (nl)
 
 ;;;
-;;;How the Reader Uses Packages
+;;; Vocabulary
 ;;;
+;;;; PACKAGE
+;;;;  A table that maps strings to symbols
+;;;;
+;;;; FIND-PACKAGE
+;;;;  Any package has a name. This function can be used to find the package by name.
+;;;;
+;;;; FIND-SYMBOL/INTERN
+;;;;  Functions that the reader uses to access the name-to-symbol mappings in a package.
+;;;;  FIND-SYMBOL looks in the package for a symbol with the given string and returns it.
+;;;;  INTERN will return an existing symbol; otherwise it creates a new symbol with the string
+;;;;         as its name and adds it to the package.
+;;;;
+;;;; UNQUALIFIED SYMBOL NAME
+;;;;  Names that contain no colons.
+;;;;
+;;;; PACKAGE QUALIFIED NAME
+;;;;  A name containing either a single colon or a double colon is a PACKAGE QUALIFIED NAME.
+;;;;  
+
+;;;
+;;; How the Reader Uses Packages
+;;;
+
+
 
 *package*     ;; The current package
 (format t "The current package is ~a " *package*)
