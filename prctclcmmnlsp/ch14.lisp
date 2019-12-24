092902713280170a52c016b0b17ebcb540d24845
szskr <seizosakurai@mac.com>
2019-12-24 15:48:34 +0900

    IMPRO

--- prctclcmmnlsp/ch14.lisp
+++ prctclcmmnlsp/ch14.lisp
@@ -6,3 +6,18 @@
 (chap14)
 (comment "Chapter 14")
 
+;;;
+;;; Reading File Data
+;;;
+(defvar ch14.01.txt "./ch14.01.txt")
+
+(nl)
+(comment "Opening a text file:")
+(format t "(setf fin (open ch14.01.txt)) = ~a~%" (setf fin (open ch14.01.txt)))
+fin
+
+(nl)
+(format t "Read a text line from ~a~%" fin)
+(let ((in (open ch14.01.txt)))
+  (format t "~a ~%" (read-line in))
+  (close in))
