45ee342d062d9bf52397507e9e314e6e2c6aee42
szskr <seizosakurai@mac.com>
2020-01-02 21:07:56 +0900

    IMPRO

--- prctclcmmnlsp/ch24-intro.lisp
+++ prctclcmmnlsp/ch24-intro.lisp
@@ -24,7 +24,17 @@
 	:element-type '(unsigned-byte 8)))
 
 (defun cat (file)
-  ())
+  (let ((in (open-r-binary file))
+	(byte 0))
+    (when (eq in nil)
+      (format t "cat: ~a can not be open~%" file)
+      (return-from cat nil))
+    (loop
+     (setf byte (read-byte in nil))
+     (format t "0x~x~%" byte)
+     (when (eq byte nil)
+       (return)
+    (close in)))))
 
 (defun cp (src dst)
   (let ((in (open-r-binary src))
@@ -37,8 +47,28 @@
       (format t "cp: ~a can not be open~%" dst)
       (close in)
       (return-from cp nil))
-    (setf byte (read-byte in))
-    (write-byte byte out)
+    (loop
+     (setf byte (read-byte in nil))
+     (if (not (eq byte nil))	 
+	 (write-byte byte out))
+     (when (eq byte nil)
+       (return)))
     (close in)
     (close out)))
-	
+
+;;
+;; Using with-open-file function
+;;
+(defun byte-copy (src dst)
+  (with-open-file (in src
+		      :direction :input
+		      :element-type '(unsigned-byte 8)
+		      :if-does-not-exist nil)
+		  (when in
+		    (with-open-file (out dst
+					 :direction :output
+					 :element-type '(unsigned-byte 8)
+					 :if-exists :supersede)
+				    (loop for byte = (read-byte in nil)
+					  while byte
+					  do (write-byte byte out))))))
