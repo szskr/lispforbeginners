fb8ab0014f5887d0c42c471699393d54e561fd02
szskr <seizosakurai@mac.com>
2020-01-04 12:06:29 +0900

    IMPRO

--- prctclcmmnlsp/ch24.lisp
+++ prctclcmmnlsp/ch24.lisp
@@ -12,7 +12,7 @@
 ;; Binary Format Basics
 ;;
 
-(defun read-u2 (in)
+(defun read-u2-0 (in)
   (+ (* (read-byte in) 256) (read-byte in)))
 
 (format t "(ldb (byte 8 0) #xabcd) = 0x~x~%" (ldb (byte 8 0) #xabcd))
@@ -32,4 +32,30 @@
 (format t "(setf (ldb (byte 8 8) *num*) #xab) = 0x~x~%" (setf (ldb (byte 8 8) *num*) #xab))
 (format t "*num* = 0x~x~%" *num*)
 (nl)
+
+(defun read-u2 (in)
+  (let ((u2 0))
+    (setf (ldb (byte 8 8) u2) (read-byte in))
+    (setf (ldb (byte 8 0) u2) (read-byte in))
+    u2))
+
+(defun write-u2 (out value)
+  (write-byte (ldb (8 8) value) out)
+  (write-byte (ldb (8 0) value) out))
+
+;;;
+;;; Strings in Binary Files
+;;;
+(defconstant +null+ (code-char 0))
+
+(defun read-null-terminated-ascii (in)
+  (with-output-to-string (s)
+			 (loop for char = (code-char (read-byte in))
+			       until (char= char +null+) do (write-char char s))))
+
+(defun write-null-terminated-ascii (string out)
+  (loop for char across string
+	do (write-byte (char-code char) out))
+  (write-byte (char-code +null+) out))
+  
 
