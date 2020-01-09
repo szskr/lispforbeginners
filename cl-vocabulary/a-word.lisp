d593c7e7757507b2d9036a8ba5debced1f2ff6c8
szskr <seizosakurai@mac.com>
2020-01-09 15:54:22 +0900

    IMPRO

--- cl-vocabulary/a-word.lisp
+++ cl-vocabulary/a-word.lisp
@@ -8,21 +8,22 @@
 (defclass root ()
   ((root
     :initarg :root)
-   (definition
-     :initarg :definition)
-   (words
-    :initarg :words)))
+   (def
+     :initarg :def)
+   (wds
+    :initarg :wds)))
 
 (defclass a-word ()
-  ((a-word
-    :initarg :a-word)
-   (definition
-     :initarg :definition)
-   (examples
-    :initarg :examples)
+  ((wd
+    :initarg :wd
+    :initform nil)
+   (def
+     :initarg :def)
+   (exs
+    :initarg :exs)
    (root
     :initarg :root)
-   (comments
+   (cmmts
     :initarg :comments)))
 
 ;;;
@@ -30,11 +31,27 @@
 ;;;
 (defparameter *w1*
   (make-instance 'a-word
-		 :a-word "Tokyo"
-		 :examples '("Tokyo is the capital of Japan."
-			     "2020 Olympic will be held in Tokyo")))
+		 :wd "Tokyo"
+		 :exs '("Tokyo is the capital of Japan."
+			"2020 Olympic will be held in Tokyo")))
 (defparameter *w2*
   (make-instance 'a-word
-		 :a-word "Sapporo"
-		 :examples '("Sapporo is the biggest city in Hokkaido."
-			     "2022 Winter Olympic will be held in Sapporo?")))
+		 :wd "Sapporo"
+		 :exs '("Sapporo is the biggest city in Hokkaido."
+			"2022 Winter Olympic will be held in Sapporo?")))
+
+;;;
+;;; slot-value
+;;;
+(format t "(slot-value *w1* 'wd) = ~a~%" (slot-value *w1* 'wd))
+(format t "(slot-value *w1* 'exs) = ~a~%" (slot-value *w1* 'exs))
+(comment "Adding an example sentence")
+(setf (slot-value *w1* 'exs) (append (slot-value *w1* 'exs) '("1964 Olympic was held in Tokyo too")))
+(format t "(slot-value *w1* 'exs) = ~a~%" (slot-value *w1* 'exs))
+
+;;;
+;;; setf function
+;;;
+(defparameter *w3* (make-instance 'a-word))
+
+(defgeneric (setf wd) (value a-word))
