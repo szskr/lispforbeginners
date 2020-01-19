089391c9e5722e8e06f3e7ba94ef12c2937a9b75
szskr <seizosakurai@mac.com>
2020-01-19 11:31:10 +0900

    IMPRO

--- prctclcmmnlsp/init.lisp
+++ prctclcmmnlsp/init.lisp
@@ -22,6 +22,7 @@
 		 "./ch21.lisp"
 		 "./ch23.lisp"
 		 "./ch24.lisp"
+		 "./x-maps.lisp"
 		 "./x-methods.lisp"
 		 "./y-ch24.lisp"
 		 "./z-appndx-ch24.lisp")))
@@ -32,6 +33,9 @@
 
 (defun x-methods ()
   (load "./x-methods.lisp"))
+
+(defun x-maps ()
+  (load "./x-maps.lisp"))
 
 (defun ch24 ()
   (load "./ch24.lisp"))
