2e4e66aaec174796e1e5d8463805b204768028ea
szskr <seizosakurai@mac.com>
2020-01-21 13:25:32 +0900

    IMPRO

---
+++ onlisp/ch02.lisp
@@ -0,0 +1,15 @@
+;;
+;; Chapter 02: Functions
+;;
+(comment "Chap02: Functions")
+(nl)
+
+(comment "02.09 Compile")
+(nl)
+
+(defun foo (x) (+ 1 x))
+
+(myformat "(compiled-function-p #'foo) = ~a~%" (compiled-function-p #'foo))
+(compile 'foo)
+
+(comment "SZSKR:2020/01/21 It seems that in sbcl, defined functions are by default 'COMPILED'. ?")
--- onlisp/init.lisp
+++ onlisp/init.lisp
@@ -2,8 +2,12 @@
 ;;; Little tools
 ;;;
 (defun l ()
-  (let ((files '("../libs/tools.lisp")))
+  (let ((files '("../libs/tools.lisp"
+		 "./ch02.lisp")))
     (mapcar #'load files)))
 
 (defun tools()
   (load "../libs/tools.lisp"))
+
+(defun ch02()
+  (load "./ch02.lisp"))
