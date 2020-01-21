4c480b9efdb971733e158596a2d309fcd9c2bdb7
szskr <seizosakurai@mac.com>
2020-01-22 00:36:12 +0900

    IMPRO

--- prctclcmmnlsp/x-values.lisp
+++ prctclcmmnlsp/x-values.lisp
@@ -4,3 +4,12 @@
 
 (comment "Values/Multiple-values-bind and their family")
 (nl)
+
+(comment "The function truncate returns two values.~%")
+(comment "The multiple-value-bind() macro can be used to capture multi-values returned.")
+(comment "Here is an example:")
+(format t "(truncate 17.1234567) = ~a~%" (truncate 17.123))
+(format t "(multiple-value-bind (int frac) (truncate 17.123)
+		      (list int frac)) = ~a~%"
+	(multiple-value-bind (int frac) (truncate 17.123)
+			     (list int frac)))
