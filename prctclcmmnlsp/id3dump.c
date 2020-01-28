9bc17e9ca09bdbd33e3374332da21229022fbecb
szskr <seizosakurai@mac.com>
2020-01-28 13:55:27 +0900

    IMPRO

---
+++ prctclcmmnlsp/id3dump.c
@@ -0,0 +1,32 @@
+/*
+ * Dump ID3 format file
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+  printf("Hello World\n");
+
+  /*
+   * Analyze options
+   */
+
+  /*
+   * Open files
+   */
+
+  /*
+   * Analyze the file
+   */
+
+  /*
+   * Print out the contents of the file
+   */
+
+  /*
+   * Close
+   */
+  
+}
