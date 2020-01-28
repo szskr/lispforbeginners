7f257f5ec8247b264acdc8958c4cc1c7be0004b5
szskr <seizosakurai@mac.com>
2020-01-28 14:41:27 +0900

    IMPRO

--- prctclcmmnlsp/id3dump.c
+++ prctclcmmnlsp/id3dump.c
@@ -4,6 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct header {
+  char id[3]; /* "ID3" */
+  unsigned char version[2];
+  unsigned char flags[4];
+  unsigned int size;
+} Header;
+
+typedef struct extended_header {
+  unsigned int size;
+  unsigned char num_of_flag_bytes;
+  unsigned char extended_flags;
+} Extended_header;
+  
 int
 main(int argc, char *argv[])
 {
