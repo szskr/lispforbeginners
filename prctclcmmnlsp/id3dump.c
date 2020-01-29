4618812f716a447bbdcdbf7a88f0d7bb01dfb674
szskr <seizosakurai@mac.com>
2020-01-29 13:46:41 +0900

    IMPRO

--- prctclcmmnlsp/id3dump.c
+++ prctclcmmnlsp/id3dump.c
@@ -7,16 +7,24 @@
 int
 main(int argc, char *argv[])
 {
-  printf("Hello World\n");
+  Id3_tag *id3_tag;
+  char *fname;
 
+  fname = argv[1];
+  
   /*
    * Analyze options
    */
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s files,,,\n", argv[0]);
+    exit (1);
+  }
 
   /*
    * Open files
    */
-
+  id3_tag = id3_open(fname);
+  
   /*
    * Analyze the file
    */
