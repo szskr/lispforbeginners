41e7ca885eea1c88b5c0da591173aa493b8b52d0
szskr <seizosakurai@mac.com>
2020-01-29 11:27:18 +0900

    IMPRO

--- prctclcmmnlsp/id3.h
+++ prctclcmmnlsp/id3.h
@@ -1,20 +1,44 @@
 /*
- * ID3 tag version 2.4.0
+ * ID3 tag version 2.4.
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct header {
-  char id[3]; /* "ID3" */
-  unsigned char version[2];
-  unsigned char flags[4];
-  unsigned int size;
-} Header;
+typedef unsigned char uchar;
+typedef unsigned int uint;
 
-typedef struct extended_header {
-  unsigned int size;
-  unsigned char num_of_flag_bytes;
-  unsigned char extended_flags;
-} Extended_header;
-  
+typedef struct id3 Id3;
+typedef struct header Header;
+typedef struct extended_header Extended_header;
+typedef struct footer Footer;
+
+struct id3 {
+  int fd;
+  uchar *mmapped;
+  Header *header;
+  Extended_header *extended_header;
+  uchar *frames;
+  uchar *paddings;
+  Footer *footer;
+};
+
+struct header {
+  char id[3];
+  uchar version[2];
+  uchar flags;
+  uint size;
+};
+
+struct extended_header {
+  uint size;
+  uchar num_of_flag_bytes;
+  uchar extended_flags;
+};
+
+struct footer {
+  char id[3];
+  uchar version;
+  uchar flags;
+  uint size;
+};
