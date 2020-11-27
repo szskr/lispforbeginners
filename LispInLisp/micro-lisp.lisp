d595ab5d53c89895559cff5b3ef0de7f2dd3edba
szskr <seizosakurai@mac.com>
2020-11-27 22:08:41 +0900

    IMPRO

---
+++ LispInLisp/micro-lisp.lisp
@@ -0,0 +1,53 @@
+;;
+;; MICRO Lisp
+;;
+(defun micro-eval (s environment)
+  (cond ((atom s)
+	 (cond ((equal s t) t)
+	       ((equal s nil) nil)
+	       ((numberp s) s)
+	       (t (micro-value s environment))))
+	((equal (car s) 'm-quote) (cadr s))
+	((equal (car s) 'm-cond)
+	 (micro-evalcond (cdr s) environment))
+	(t (micro-apply (car s)
+			(mapcar '(lambda (x)
+				   (micro-eval x environment))
+				(cdr s))
+			environment))))
+
+(defun micro-apply (function args envirnment)
+  (cond ((atom function)
+	 (cond ((equal function 'm-car) (caar args))
+	       ((equal function 'm-cdr) (cdar args))
+	       ((equal function 'm-cons) (cons (car args)
+					       (cadr args)))
+	       ((equal function 'm-atom) (atom (car args)))
+	       ((equal function 'm-null) (null (car args)))
+	       ((equal function 'm-equal) (equal (car args)
+						 (cadr args)))
+	       (t (micro-apply
+		   (micro-eval function environment)
+		   args
+		   environment))))
+	((equal (car function) 'm-definition)
+	 (micro-eval (caddr function)
+		     (micro-bind (cadr function) args environment)))))
+
+(defun microp-rep ()
+  (prog (s enviroment)
+	loop
+	(setq s (read))
+	(cond ((atom s)
+	       (print (micro-eval s environment)))
+	      ((equal (car s) 'm-defun)
+	       (setq environment
+		     (cons (list (cadr s)
+				 (cons 'm-definition
+				       (cddr s)))
+			   environment))
+	       (print (cadr s)))
+	      (t (print (micro-eval s environment))))
+	(go loop)))
+	       
+	       
---
+++ LispInLisp/tiny-lisp.lisp
@@ -0,0 +1,1 @@
+../lispforbeginners/tiny-lisp.lisp
