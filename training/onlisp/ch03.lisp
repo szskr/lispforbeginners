0662463c38f6122dcc7f511c2ad60db323295d6f
szskr <seizosakurai@mac.com>
2021-01-04 11:42:33 +0900

    IMPRO

--- training/onlisp/ch03.lisp
+++ training/onlisp/ch03.lisp
@@ -7,3 +7,21 @@
 (comment "03.01 Functional Programming Design")
 
 (myformat "(truncate 17.123) = ~a~%" (truncate 17.123))
+
+;;
+;; Diag 3.2
+;;
+(defun good-reverse (lst)
+  (labels ((rev (tlst acc)
+		(if (null tlst)
+		    acc
+		  (rev (cdr tlst) (cons (car tlst) acc)))))
+	  (rev lst nil)))
+
+;;
+;;
+(defun ok (x)
+  (nconc (list 'a x) (list 'c)))
+
+(defun not-ok (x)
+  (nconc (list 'a) x (list 'c)))   ;; THis is not ok because it modifies the argument passed.
