2e3e77d3d741e7e904385c0485ba7e4bce12dced
szskr <seizosakurai@mac.com>
2021-01-08 16:14:05 +0900

    IMPRO

--- training/onlisp/maps.lisp
+++ training/onlisp/maps.lisp
@@ -29,10 +29,13 @@
 (setq l3 (apply #'list '(1 2 3 (a b))))
 
 ;;
-;; maps - mapcar, maplist
+;; maps - mapcar, maplist, mapc, 
 ;;
 (setq nums '(1 2 3 4 5 6 7 8 9 10))
 
+;;
+;; mapcar
+;;
 (setq m0 (mapcar #'+ nums))
 (setq m1 (mapcar #'+ nums nums))
 (setq m2 (mapcar #'(lambda (x) (* x x)) nums))
@@ -59,3 +62,40 @@
 		   '(1 2 3 4)))
 
 (setq ml2 (maplist (lambda (l) (+ 100 (car l))) nums))
+
+;;
+;; A simple version of maplist
+;;
+(defun my-maplist (fn mlist)
+  (cond ((null mlist) nil)
+	(t (cons (funcall fn mlist)
+		 (my-maplist fn (cdr mlist)) ))))
+
+(setq mml0 (my-maplist (lambda (l) (cons 0 l)) '(1 2 3 4)))
+
+;;(setq mml1 (my-maplist #'append
+;;		   '(a b c d - )
+;;		   '(1 2 3 4)))
+
+(setq ml2 (maplist (lambda (l) (+ 100 (car l))) nums))
+
+;;
+;; mapc
+;;
+(setq mc0 (mapc #'+ nums))
+(setq mc1 (mapc #'+ nums nums))
+(write "Going to mapc square")
+(setq mc2 (mapc #'(lambda (x) (print (* x x)) (* x x)) nums))
+
+;;
+;; A simple version of mapcar
+;;
+(defun my-mapc (fn mlist)
+  (do ((x mlist (cdr x)))
+      ((null x) mlist)
+      (funcall fn (car x))))
+
+(setq mmc0 (my-mapc #'+ nums))
+;(setq mmc1 (my-mapc #'+ nums nums))
+(write "Going to my-mapc square")
+(setq mc2 (my-mapc #'(lambda (x) (print (* x x)) (* x x)) nums))
